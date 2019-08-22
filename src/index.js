5978362085018034ac3cce5be04039f0b7e58f6b
Simon Palmqvist <simon.palmqvist@gmail.com>
2019-08-22 23:01:08 +0200

    Fixing linting

--- src/index.js
+++ src/index.js
@@ -1,65 +1,65 @@
-import React, { useState, useContext, useMemo } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useContext, useMemo } from 'react'
+import ReactDOM from 'react-dom'
 
-function createStore(updateValue) {
-	const Context = React.createContext({});
+function createStore (updateValue) {
+  const Context = React.createContext({})
 
-	return [
-		Context,
-		({ children }) => (
-			<Context.Provider value={updateValue()}>{children}</Context.Provider>
-		)
-	];
+  return [
+    Context,
+    ({ children }) => (
+      <Context.Provider value={updateValue()}>{children}</Context.Provider>
+    )
+  ]
 }
 
 const withContext = (Context, values) => Component => props => {
-	const state = useContext(Context);
+  const state = useContext(Context)
 
-	const newProps = {};
+  const newProps = {}
 
-	values.forEach(value => {
-		newProps[value] = state[value];
-	});
+  values.forEach(value => {
+    newProps[value] = state[value]
+  })
 
-	return useMemo(
-		() => <Component {...props} {...newProps} />,
-		Object.values(newProps)
-	);
-};
+  return useMemo(
+    () => <Component {...props} {...newProps} />,
+    Object.values(newProps)
+  )
+}
 
 const [NameContext, NameProvider] = createStore(() => {
-	const [name, setName] = useState("Simon");
-	return { name, setName, quote: "Hehehe" };
-});
+  const [name, setName] = useState('Simon')
+  return { name, setName, quote: 'Hehehe' }
+})
 
-function Header() {
-	const { name } = useContext(NameContext);
-	console.log("Header re-rendering");
-	return <h1>{name}</h1>;
+function Header () {
+  const { name } = useContext(NameContext)
+  console.log('Header re-rendering')
+  return <h1>{name}</h1>
 }
 
-function View({ name, setName }) {
-	console.log("View re-rendering");
-	return <input value={name} onChange={evt => setName(evt.target.value)} />;
+function View ({ name, setName }) {
+  console.log('View re-rendering')
+  return <input value={name} onChange={evt => setName(evt.target.value)} />
 }
 
-const ViewContainer = withContext(NameContext, ["name", "setName"])(View);
+const ViewContainer = withContext(NameContext, ['name', 'setName'])(View)
 
 const View2 = ({ quote }) => {
-	console.log("View2 re-rendering");
-	return <p>{quote}</p>;
-};
-
-const View2Container = withContext(NameContext, ["quote"])(View2);
-
-function App() {
-	return (
-		<NameProvider>
-			<Header />
-			<ViewContainer />
-			<View2Container />
-		</NameProvider>
-	);
+  console.log('View2 re-rendering')
+  return <p>{quote}</p>
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const View2Container = withContext(NameContext, ['quote'])(View2)
+
+function App () {
+  return (
+    <NameProvider>
+      <Header />
+      <ViewContainer />
+      <View2Container />
+    </NameProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
